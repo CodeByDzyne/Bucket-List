208fff385f3375061c3d50d090b447b18b0a6ff6
CodeByDzyne <wdenley@dzynesolutions.com>
2023-02-20 18:16:43 +1100

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Wealthy Waz on 20/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bucket List/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bucket List
+//
+//  Created by Wealthy Waz on 20/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
